b69b4f756d5a3692c66b103cac396fef95a0ae27
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-10-10 18:44:09 +0200

    adding redux provider

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -2,28 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Routes, Route } from 'react-router-dom'
-import { ViewportProvider } from './utils/Context/ViewportContext'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import GlobalStyle from './utils/styles/GlobalStyle'
 import Footer from './components/Footer'
 import User from './pages/User'
+import { Provider } from 'react-redux'
+import { store } from './utils/redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-	<React.StrictMode>
+	<Provider store={store}>
 		<BrowserRouter basename={process.env.PUBLIC_URL}>
-			<ViewportProvider>
-				<GlobalStyle />
-				<Navbar />
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/profile" element={<User />} />
-				</Routes>
-				<Footer />
-			</ViewportProvider>
+			<GlobalStyle />
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/profile" element={<User />} />
+			</Routes>
+			<Footer />
 		</BrowserRouter>
-	</React.StrictMode>
+	</Provider>
 )
+
