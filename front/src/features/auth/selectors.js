57184559c249438f753313a11233ff4bd65147ff
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-10-15 12:26:28 +0200

    delete moved or renamed files

--- front/src/features/auth/selectors.js
+++
@@ -1,5 +0,0 @@
-export const getAuthenticationResponse = (state) => state.authentication
-
-export const getStatus = (state) => state.authentication.status
-
-export const getTokenState = (state) => state.authentication.user.token
