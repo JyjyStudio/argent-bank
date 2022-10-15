2877a93dd1463f898fddf98ee5d8be19eebb23a3
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-10-15 12:32:03 +0200

    feat: navbar binded to the store for username and logged state

--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -4,8 +4,12 @@ import Img from './Img'
 import Logo from '../assets/argentBankLogo.png'
 import SignInIcon from '../assets/icon-login.png'
 import SignOutIcon from '../assets/icon-logout.png'
-import { useDispatch } from 'react-redux'
-import { logOut } from '../features/auth/authSlice'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { resetAuth } from '../features/authentication/authenticationSlice'
+import { asyncGetInfos, resetUser } from '../features/user/userSlice'
+import { getTokenFromState } from '../features/authentication/selectors'
+import { getUserInfosFromState } from '../features/user/selectors'
 
 /**
  * The top and left navbar
@@ -14,15 +18,20 @@ import { logOut } from '../features/auth/authSlice'
  * @component
  */
 export default function Navbar() {
-
 	// user token from redux store
-	const userToken = localStorage.getItem('userToken')
+	const userToken = useSelector(getTokenFromState)
+	const userInfos = useSelector(getUserInfosFromState)
 
-	// redirect user to profile page if login was successful
 	const dispatch = useDispatch()
-	const loggOut = () => {
-		localStorage.removeItem('userToken')
-		dispatch(logOut())
+
+	useEffect(() => {
+		userToken && dispatch(asyncGetInfos(userToken))
+	}, [dispatch, userToken])
+
+
+	const resetCredentials = () => {
+		dispatch(resetAuth())
+		dispatch(resetUser())
 	}
 
 	return (
@@ -30,23 +39,40 @@ export default function Navbar() {
 			<StyledLink to="/">
 				<Img src={Logo} width="200px" alt="logo argentBank" />
 			</StyledLink>
-			{/* if user connected return name + logout else return login */
-			userToken ?
-				<SignOutContainer>
-					<NavLink to="/profile">
-						<Img src={SignInIcon} width="25px" margin="0 5px 0 0" alt="sign in" />
-						<Span>Prenom</Span>
+			{
+				/* if user connected return name + logout else return login */
+				userToken ? (
+					<SignOutContainer>
+						<NavLink to="/profile">
+							<Img
+								src={SignInIcon}
+								width="25px"
+								margin="0 5px 0 0"
+								alt="sign in"
+							/>
+							<Span>{userInfos.firstname}</Span>
+						</NavLink>
+						<NavLink onClick={resetCredentials}>
+							<Img
+								src={SignOutIcon}
+								width="20px"
+								margin="0 5px 0 0"
+								alt="sign in"
+							/>
+							<Span>Sign Out</Span>
+						</NavLink>
+					</SignOutContainer>
+				) : (
+					<NavLink to="/login">
+						<Img
+							src={SignInIcon}
+							width="25px"
+							margin="0 5px 0 0"
+							alt="sign in"
+						/>
+						<Span>Sign In</Span>
 					</NavLink>
-					<NavLink onClick={() => loggOut()}>
-						<Img src={SignOutIcon} width="20px" margin="0 5px 0 0" alt="sign in" />
-						<Span>Sign Out</Span>
-					</NavLink>
-				</SignOutContainer>
-				:
-				<NavLink to="/login">
-					<Img src={SignInIcon} width="25px" margin="0 5px 0 0" alt="sign in" />
-					<Span>Sign In</Span>
-				</NavLink>
+				)
 			}
 		</Nav>
 	)
