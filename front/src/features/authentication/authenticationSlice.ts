ce834cc3f6ecd8084b551d13722b50589d149491
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-10-17 16:53:45 +0200

    fix / clean ts code

--- front/src/features/authentication/authenticationSlice.ts
+++ front/src/features/authentication/authenticationSlice.ts
@@ -1,57 +1,69 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import axios from 'axios'
-import React from 'react'
-import { RootState } from '../../utils/redux/store'
+import axios, { AxiosError } from 'axios'
+import { AppDispatch } from '../../utils/redux/store'
+
+interface Resolved {
+	status: number,
+	message: string,
+	body: {token: string}
+}
+interface Rejected {
+	error?: AxiosError
+	displayedError?: string
+}
+interface UserCredentials { email: string; password: string }
+
+interface InitialState {
+	status: string
+	response: null | Resolved & Rejected
+	token: null | string
+}
 
 //creation d'une partie de notre store (slice). Contient le nom du slice, son state initial et les reducers+
-const initialState = {
+const initialState: InitialState = {
 	status: 'void',
 	response: null,
-	error: null,
-	logged: false,
 	token: null,
 }
 
-export const authenticationSlice: RootState = createSlice({
+export const authenticationSlice = createSlice({
 	name: 'auth',
 	initialState,
 	reducers: {
 		// la requête est en cours
-		fetching: (state: RootState) => {
+		fetching: (state) => {
 			// si le statut est void on passe le status en pending
 			if (state.status === 'void') {
 				state.status = 'pending'
 			}
 			// si le statut est rejected on supprime l'erreur et on passe en updating
 			if (state.status === 'rejected') {
-				state.error = null
-				state.status = 'updating'
+				state.response = null
+				state.status = 'pending'
 			}
 			// si le statut est resolved on passe en updating (requête en cours mais des données sont déjà présentes)
 			if (state.status === 'resolved') {
 				state.status = 'updating'
-				state.logged = true
 			}
 		},
 		// la requête a fonctionné
-		resolved: (state: RootState, action: PayloadAction<Resolved> ) => {
+		resolved: (state, action: PayloadAction<Resolved> ) => {
 			// si la requête est en cours on passe en resolved et on sauvegarde les données
 			if (state.status === 'pending' || state.status === 'updating') {
 				state.status = 'resolved'
 				state.response = action.payload
 				state.token = action.payload.body.token
-				state.logged = true
 			}
 		},
 		// la requête a échoué
-		rejected: (state: RootState, action: PayloadAction<string>) => {
+		rejected: (state, action: PayloadAction<string>) => {
 			// si la requête est en cours on passe en rejected, on sauvegarde l'erreur
 			if (state.status === 'pending' || state.status === 'updating') {
 				state.status = 'rejected'
-				state.error = JSON.parse(action.payload)
+				state.response = JSON.parse(action.payload)
 			}
 		},
-		resetAuth: (state: RootState) => initialState,
+		resetAuth: (state) => initialState,
 	},
 })
 
@@ -59,7 +71,7 @@ export const authenticationSlice: RootState = createSlice({
 export const { fetching, resolved, rejected, resetAuth } = authenticationSlice.actions
 
 // async fetching to get token
-export const asyncGetToken = (userInfos: UserInfos) => async (dispatch: React.FC) => {
+export const asyncGetToken = (userInfos: UserCredentials) => async (dispatch: AppDispatch) => {
 	dispatch(fetching())
 
 	const headers = {
@@ -86,7 +98,3 @@ export const asyncGetToken = (userInfos: UserInfos) => async (dispatch: React.FC
 		}
 	}
 }
-
-
-interface Resolved { body: { token: string } }
-interface UserInfos { email: string; password: string }
--- front/src/features/user/userSlice.ts
+++ front/src/features/user/userSlice.ts
@@ -1,57 +1,56 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import axios from 'axios'
-import React from 'react'
-import { RootState } from '../../utils/redux/store'
+import { AppDispatch } from '../../utils/redux/store'
+
+interface InitialState {
+	firstname: null | string
+	lastname: null | string
+}
+interface User {
+	body: {
+		firstName: null | string
+		lastName: null | string
+	}
+}
 
 //creation d'une partie de notre store (slice). Contient le nom du slice, son state initial et les reducers+
-const initialState = {
+const initialState: InitialState = {
 	firstname: null,
 	lastname: null,
 }
-export const userSlice: RootState = createSlice({
+export const userSlice = createSlice({
 	name: 'user',
 	initialState,
 	reducers: {
-		getInfos: (state: RootState,  action: PayloadAction<GetInfos>) => {
+		getInfos: (state, action: PayloadAction<User>) => {
 			state.firstname = action.payload.body.firstName
 			state.lastname = action.payload.body.lastName
 		},
-		resetUser: (state: RootState) => initialState,
+		resetUser: (state) => initialState,
 	},
 })
 
 // actions creator
-export const { rejected, getInfos, resetUser } = userSlice.actions
+export const { getInfos, resetUser } = userSlice.actions
 
 // async fetching to user informations
-export const asyncGetInfos = (token: string) => async (dispatch: React.FC) => {
-
+export const asyncGetInfos = (token: string) => async (dispatch: AppDispatch) => {
 	const headers = {
 		accept: 'application/json',
 		Authorization: 'Bearer ' + token,
 	}
 
 	try {
+
 		const response = await axios.post('http://localhost:3001/api/v1/user/profile', token, { headers })
 		dispatch(getInfos(response.data))
+		
 	} catch (error) {
 		if (axios.isAxiosError(error) && error.response) {
-			dispatch(
-				rejected(
-					JSON.stringify({
-						error,
-						displayedError: 'incorrect email/password',
-					})
-				)
-				)
-				console.log('error from userSlice')
+			JSON.stringify({
+				error,
+				displayedError: 'incorrect email/password',
+			})
 		}
-	}
-}
-
-interface GetInfos {
-	body: { 
-		firstName: string,
-		lastName: string
 	}
 }
