896707a5a8933729cb00bb23f43b437f85a5b411
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-10-18 20:26:58 +0200

    feat: lightmode/darkmode

--- front/src/components/Footer.tsx
+++ front/src/components/Footer.tsx
@@ -1,20 +1,38 @@
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { RootState } from '../utils/redux/store'
+import { toggleTheme } from '../features/theme/themeSlice'
+import { useTsDispatch } from '../utils/redux/hooks'
 
 export default function Footer() {
 	
 	const year = new Date().getFullYear()
+	const theme = useSelector((state: RootState) => state.theme.theme)
+
+	const dispatch = useTsDispatch()
+	const toggle = () => dispatch(toggleTheme())
 
 	return (
-		<FooterContainer>
+		<FooterContainer theme={theme}>
 			<p>Copyright {year} <StyledLink to="/">Argent Bank</StyledLink></p>
+			<NightModeBtn
+				onClick={toggle}
+			>
+			<Icon theme={theme}>{theme === 'light' ? '🌙' : '☀️'}</Icon>
+			</NightModeBtn>
 		</FooterContainer>
 	)
 }
 
-const FooterContainer = styled.footer`	
-	border-top: 2px solid #ccc;
-	padding: 2rem 0 1.5rem;
+const FooterContainer = styled.footer<ThemeProps>`	
+	padding: 1.5rem;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+    margin: 0 auto;
+	background: ${({ theme }) => (theme === 'dark' ? '#ecf0f1' : '#fff')};
 `
 const StyledLink = styled(Link)`
 	color: inherit;
@@ -24,3 +42,19 @@ const StyledLink = styled(Link)`
 		text-decoration: underline;
 	}
 `
+
+const NightModeBtn = styled.button`
+	background-color: transparent;
+	font-size: 2rem;
+	border: none;
+`
+const Icon = styled.span<ThemeProps>`
+	margin-left: 1.5rem;
+	cursor: pointer;
+	&:hover {
+		filter: ${({ theme }) => (theme === 'dark' ? 'drop-shadow(0px 0px 15px #fff)' : 'drop-shadow(0px 0px 15px black)')};
+	}
+`
+interface ThemeProps {
+	theme : boolean
+}
