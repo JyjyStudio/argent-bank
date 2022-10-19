32107fdd9fcd656aa12a2be09288aaa4071effdb
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-10-19 16:25:12 +0200

    feat: userInfos request status selector

--- front/src/features/user/selectors.ts
+++ front/src/features/user/selectors.ts
@@ -1,3 +1,5 @@
 import { RootState } from "../../utils/redux/store";
 
 export const getUserInfosFromState = (state: RootState) => state.userInfos
+
+export const getUserInfosStatus = (state: RootState) => state.userInfos.status
