a9c5851ec53bf7926aed63b34c19a49e6c194c5d
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-10-20 09:35:06 +0200

    feat: createAsyncThunk

--- front/src/features/authentication/authenticationSlice.ts
+++ front/src/features/authentication/authenticationSlice.ts
@@ -1,24 +1,6 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import axios, { AxiosError } from 'axios'
-import { AppDispatch } from '../../utils/redux/store'
-import { AuthInstance } from './authInstance'
-
-interface Resolved {
-	status: number,
-	message: string,
-	body: {token: string}
-}
-interface Rejected {
-	error?: AxiosError
-	displayedError?: string
-}
-interface UserCredentials { email: string; password: string }
-
-interface InitialState {
-	status: string
-	response: null | Resolved & Rejected
-	token: null | string
-}
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit'
+import { authInstance } from './authInstance'
+import axios from 'axios'
 
 //creation d'une partie de notre store (slice). Contient le nom du slice, son state initial et les reducers+
 const initialState: InitialState = {
@@ -31,65 +13,54 @@ export const authenticationSlice = createSlice({
 	name: 'auth',
 	initialState,
 	reducers: {
-		// la requête est en cours
-		fetching: (state) => {
-			// si le statut est void on passe le status en pending
-			if (state.status === 'void') {
-				state.status = 'pending'
-			}
-			// si le statut est rejected on supprime l'erreur et on passe en updating
-			if (state.status === 'rejected') {
-				state.response = null
-				state.status = 'pending'
-			}
-			// si le statut est resolved on passe en updating (requête en cours mais des données sont déjà présentes)
-			if (state.status === 'resolved') {
-				state.status = 'updating'
-			}
-		},
-		// la requête a fonctionné
-		resolved: (state, action: PayloadAction<Resolved> ) => {
-			// si la requête est en cours on passe en resolved et on sauvegarde les données
-			if (state.status === 'pending' || state.status === 'updating') {
-				state.status = 'resolved'
-				state.response = action.payload
-				state.token = action.payload.body.token
-			}
-		},
-		// la requête a échoué
-		rejected: (state, action: PayloadAction<string>) => {
-			// si la requête est en cours on passe en rejected, on sauvegarde l'erreur
-			if (state.status === 'pending' || state.status === 'updating') {
-				state.status = 'rejected'
-				state.response = JSON.parse(action.payload)
-			}
-		},
 		resetAuth: (state) => initialState,
+	},
+	extraReducers: (builder) => {
+		builder.addCase(getToken.pending, (state) => {
+			state.status = 'pending'
+		})
+		builder.addCase(getToken.fulfilled, (state, action: PayloadAction<Resolved>) => {
+			state.status = 'resolved'
+			state.response = action.payload
+			state.token = action.payload.body.token
+		})
+		builder.addCase(getToken.rejected, (state, action: PayloadAction<any>) => {
+			state.status = 'rejected'
+			state.response = action.payload
+			console.log(action.payload)
+		})
 	},
 })
 
 // actions creator
-export const { fetching, resolved, rejected, resetAuth } = authenticationSlice.actions
+export const { resetAuth } = authenticationSlice.actions
 
 // async fetching to get token
-export const asyncGetToken = (userInfos: UserCredentials) => async (dispatch: AppDispatch) => {
-	dispatch(fetching())
-
-	try {
-		const response = await AuthInstance.post('', userInfos)
-		dispatch(resolved(response.data))
-		
-	} catch (error) {
-		
-		if (axios.isAxiosError(error) && error.response) {
-			dispatch(
-				rejected(
-					JSON.stringify({
-						error,
-						displayedError: 'incorrect email/password',
-					})
-				)
-			)
+export const getToken = createAsyncThunk(
+	'auth/getToken',
+	async (userInfos: UserCredentials, { rejectWithValue }) => {
+		try {
+			const response = await authInstance.post('', userInfos)
+			return response.data
+		} catch (error) {
+			if (axios.isAxiosError(error)) {
+				return rejectWithValue(error.response?.data.message)
+			}
 		}
 	}
+)
+
+interface Resolved {
+	status: number
+	message: string
+	body: { token: string }
+}
+interface UserCredentials {
+	email: string
+	password: string
+}
+interface InitialState {
+	status: string
+	response: any
+	token: null | string
 }
