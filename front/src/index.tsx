c85bd1945f24f867eb366a7991d7bdcea286a7e4
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-10-21 21:12:03 +0200

    disableReactDevTools in production

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -2,16 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import GlobalStyle from './utils/styles/GlobalStyle'
-import Footer from './components/Footer'
-import Profile from './pages/Profile'
 import { Provider } from 'react-redux'
 import { persistor, store } from './utils/redux/store'
 import { PersistGate } from 'redux-persist/integration/react'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Profile from './pages/Profile'
+import GlobalStyle from './utils/styles/GlobalStyle'
 import NotFount from './pages/404'
+
+// to hide the state in production
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 // testing if we have a root element before invoking ReactDOM.createRoot (typescript)
 const rootElement = document.getElementById('root')
@@ -19,20 +25,21 @@ if (!rootElement) throw new Error('Failed to find the root element')
 
 const root = ReactDOM.createRoot(rootElement)
 root.render(
-	<Provider store={store}>
-		<PersistGate loading={null} persistor={persistor}>
-			<BrowserRouter basename={process.env.PUBLIC_URL}>
-				<GlobalStyle />
-				<Navbar />
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/profile" element={<Profile />} />
-					<Route path="*" element={<NotFount />} />
-				</Routes>
-				<Footer />
-			</BrowserRouter>
-		</PersistGate>
-	</Provider>
+	<React.StrictMode>
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				<BrowserRouter basename={process.env.PUBLIC_URL}>
+					<GlobalStyle />
+					<Navbar />
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/profile" element={<Profile />} />
+						<Route path="*" element={<NotFount />} />
+					</Routes>
+					<Footer />
+				</BrowserRouter>
+			</PersistGate>
+		</Provider>
+	</React.StrictMode>
 )
-
